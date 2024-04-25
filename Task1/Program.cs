c1195172c800f1a02d0eb976054380987dd4074f
vladimir.baranov <proger.vl@gmail.com>
2024-04-26 00:14:36 +0300

    Add Task1.

---
+++ Task1/Program.cs
@@ -0,0 +1,23 @@
+﻿/*
+Задача 1: 
+    Задайте значения M и N. Напишите программу, которая выведет все натуральные 
+    числа в промежутке от M до N. Использовать рекурсию, не использовать циклы.
+*/
+
+void PrintNaturalNumbers(int startM, int endN)
+{
+    if (startM > endN)
+        return;
+
+    Console.Write($"{startM} ");
+    PrintNaturalNumbers(startM + 1, endN);
+}
+
+Console.Write("Введите число M: ");
+int userM = Convert.ToInt32(Console.ReadLine());
+
+Console.Write("Введите число N: ");
+int userN = Convert.ToInt32(Console.ReadLine());
+
+PrintNaturalNumbers(userM, userN);
+Console.WriteLine();
